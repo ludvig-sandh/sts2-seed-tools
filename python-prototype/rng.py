03efadd300563f6c924af10a3d96c69443bc3750
Ludvig Sandh <givdul11@hotmail.se>
2026-03-19 21:41:40 +0100

    feat: implement more RNG methods (gaussian + shuffles)

--- python-prototype/rng.py
+++ python-prototype/rng.py
@@ -1,8 +1,13 @@
 from dotnetrandom import DotNetRandom
+import math
+from util import get_deterministic_hash_code
 
 class Rng:
-    def __init__(self, seed: int, counter: int = 0):
+    def __init__(self, seed: int, name: str = None, counter: int = 0):
         self.seed = seed
+        if name is not None:
+            self.seed = seed + get_deterministic_hash_code(name)
+
         self.counter = 0
         self.random = DotNetRandom(seed)
 
@@ -24,3 +29,34 @@ class Rng:
     def next_float(self, min_val=0.0, max_val=1.0):
         self.counter += 1
         return self.random.next_double() * (max_val - min_val) + min_val
+    
+    def next_gaussian_int(self, mean: int, std_dev: int, min_val: int, max_val: int) -> int:
+        while True:
+            d = 1.0 - self.next_double()
+            num = 1.0 - self.next_double()
+            num2 = math.sqrt(-2.0 * math.log(d)) * math.sin(math.pi * 2.0 * num)
+            a = mean + std_dev * num2
+            value = round(a)
+            if min_val <= value <= max_val:
+                return value
+            
+    @staticmethod
+    def unstable_shuffle(lst, rng):
+        num = len(lst)
+        while num > 1:
+            num -= 1
+            j = rng.next_int(num + 1)
+            lst[j], lst[num] = lst[num], lst[j]
+        return lst
+
+    @staticmethod
+    def stable_shuffle(lst, rng):
+        try:
+            baseline = sorted(lst)
+        except TypeError:
+            baseline = list(lst)
+
+        for i in range(len(lst)):
+            lst[i] = baseline[i]
+
+        return Rng.unstable_shuffle(lst, rng)
