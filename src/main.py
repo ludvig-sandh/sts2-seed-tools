85981dd3007d743d05bf14db247e37699be8b153
Ludvig Sandh <givdul11@hotmail.se>
2026-03-19 22:02:17 +0100

    feat: prepare code skeleton for preparing a game run

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from save_manager import SaveManager
 from run_state import RunState
 from act import ActGenerator, Act
 from util import preprocess_seed
+from run_manager import RunManager
 
 from dataclasses import dataclass
 from typing import List
@@ -15,6 +16,11 @@ def start_run(seed: str):
     progress = SaveManager.load_progress_save_file()
     acts = ActGenerator.generate_act_list(seed, progress)
     run_state = RunState.create_for_new_run(seed)
+
+    RunManager.populate_relics(run_state)
+    RunManager.generate_rooms(run_state)
+    RunManager.enter_act(run_state, 0)
+    
     return RunResult(acts)
     
 
---
+++ src/run_manager.py
@@ -0,0 +1,12 @@
+class RunManager:
+    @staticmethod
+    def populate_relics(run_state):
+        pass
+
+    @staticmethod
+    def generate_rooms(run_state):
+        pass
+
+    @staticmethod
+    def enter_act(run_state, act_idx):
+        pass
