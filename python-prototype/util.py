96f1eb9a36760446d0f35c91d58d520db38684d6
Ludvig Sandh <givdul11@hotmail.se>
2026-03-19 20:35:43 +0100

    refactor: extract helper methods into util module

---
+++ python-prototype/util.py
@@ -0,0 +1,15 @@
+def int32(x: int) -> int:
+    return ((x + 2**31) % 2**32) - 2**31
+
+
+def get_deterministic_hash_code(s: str) -> int:
+    num = 352654597
+    num2 = num
+
+    for i in range(0, len(s), 2):
+        num = int32(num * 33 ^ ord(s[i]))
+        if i == len(s) - 1:
+            break
+        num2 = int32(num2 * 33 ^ ord(s[i + 1]))
+
+    return int32(num + int32(num2 * 1566083941))
