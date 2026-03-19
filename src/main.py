117bfa070e82ec3a737e85e151d4bee94cc81803
Ludvig Sandh <givdul11@hotmail.se>
2026-03-19 23:59:31 +0100

    feat: add more relevant fields to run state class

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from run_state import RunState
 from act import ActGenerator, Act
 from util import preprocess_seed
 from run_manager import RunManager
+from character import Character
 
 from dataclasses import dataclass
 from typing import List
@@ -15,12 +16,12 @@ def start_run(seed: str):
     seed = preprocess_seed(seed)
     progress = SaveManager.load_progress_save_file()
     acts = ActGenerator.generate_act_list(seed, progress)
-    run_state = RunState.create_for_new_run(seed)
+    run_state = RunState.create_for_new_run(seed, progress, Character.IRONCLAD)  # TODO: Don't hardcode ironclad
 
     RunManager.populate_relics(run_state)
     RunManager.generate_rooms(run_state)
     RunManager.enter_act(run_state, 0)
-    
+
     return RunResult(acts)
     
 
--- src/run_state.py
+++ src/run_state.py
@@ -1,14 +1,21 @@
 from run_rng_set import RunRngSet
 from unknown_map_point_odds import UnknownMapPointOdds
+from progress import Progress
+from character import Character
 
 class RunState:
-    def __init__(self, rng_set, odds):
+    def __init__(self, rng_set: RunRngSet, odds, progress: Progress, character: Character):
         self.rng_set = rng_set
         self.odds = odds
+        self.progress = progress
+        self.character = character
+        
+        self.shared_relic_grab_bag = dict()
+        self.player_relic_grab_bag = dict()
 
     @staticmethod
-    def create_for_new_run(seed: str):
+    def create_for_new_run(seed: str, progress: Progress, character: Character):
         rng_set = RunRngSet(seed)
         odds = UnknownMapPointOdds(rng_set.unknown_map_point)
-        run_state = RunState(rng_set, odds)
+        run_state = RunState(rng_set, odds, progress, character)
         return run_state
