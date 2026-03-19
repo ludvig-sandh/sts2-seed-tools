d188cc3af323b23a1280a863283a5caaf5d959f2
Ludvig Sandh <givdul11@hotmail.se>
2026-03-20 00:00:02 +0100

    feature: implement relic grab bag generation

--- src/progress.py
+++ src/progress.py
@@ -1,4 +1,8 @@
+from typing import List
+
 from act import Act
+from relics import get_all_relics
+
 
 class Progress:
     def __init__(self):
@@ -40,3 +44,89 @@ class Progress:
     
     def is_epoch_discovered(self, epoch: str) -> bool:
         return epoch in self.discovered_epochs
+    
+    def get_locked_relics(self) -> List[str]:
+        locked_relics = set()
+
+        # Shared relics
+        if not self.is_epoch_discovered("RELIC1_EPOCH"):
+            locked_relics.add("UnsettlingLamp")
+            locked_relics.add("IntimidatingHelmet")
+            locked_relics.add("ReptileTrinket")
+
+        if not self.is_epoch_discovered("RELIC2_EPOCH"):
+            locked_relics.add("BookOfFiveRings")
+            locked_relics.add("IceCream")
+            locked_relics.add("Kusarigama")
+
+        if not self.is_epoch_discovered("RELIC3_EPOCH"):
+            locked_relics.add("VexingPuzzlebox")
+            locked_relics.add("RippleBasin")
+            locked_relics.add("FestivePopper")
+
+        if not self.is_epoch_discovered("RELIC4_EPOCH"):
+            locked_relics.add("MiniatureCannon")
+            locked_relics.add("TungstenRod")
+            locked_relics.add("WhiteStar")
+
+        if not self.is_epoch_discovered("RELIC5_EPOCH"):
+            locked_relics.add("TinyMailbox")
+            locked_relics.add("JossPaper")
+            locked_relics.add("BeatingRemnant")
+
+        # Ironclad relics
+        if not self.is_epoch_discovered("IRONCLAD3_EPOCH"):
+            locked_relics.add("RedSkull")
+            locked_relics.add("PaperPhrog")
+            locked_relics.add("RuinedHelmet")
+
+        if not self.is_epoch_discovered("IRONCLAD6_EPOCH"):
+            locked_relics.add("SelfFormingClay")
+            locked_relics.add("CharonsAshes")
+            locked_relics.add("DemonTongue")
+
+        # Silent relics
+        if not self.is_epoch_discovered("SILENT3_EPOCH"):
+            locked_relics.add("ToughBandages")
+            locked_relics.add("PaperKrane")
+            locked_relics.add("Tingsha")
+
+        if not self.is_epoch_discovered("SILENT6_EPOCH"):
+            locked_relics.add("TwistedFunnel")
+            locked_relics.add("SneckoSkull")
+            locked_relics.add("HelicalDart")
+
+        # Regent relics
+        if not self.is_epoch_discovered("REGENT3_EPOCH"):
+            locked_relics.add("FencingManual")
+            locked_relics.add("GalacticDust")
+            locked_relics.add("LunarPastry")
+            
+        if not self.is_epoch_discovered("REGENT6_EPOCH"):
+            locked_relics.add("Regalite")
+            locked_relics.add("MiniRegent")
+            locked_relics.add("OrangeDough")
+
+        # Necrobinder relics
+        if not self.is_epoch_discovered("NECROBINDER3_EPOCH"):
+            locked_relics.add("BoneFlute")
+            locked_relics.add("FuneraryMask")
+            locked_relics.add("BookRepairKnife")
+            
+        if not self.is_epoch_discovered("NECROBINDER6_EPOCH"):
+            locked_relics.add("IvoryTile")
+            locked_relics.add("BigHat")
+            locked_relics.add("Bookmark")
+
+        # Defect relics
+        if not self.is_epoch_discovered("DEFECT3_EPOCH"):
+            locked_relics.add("DataDisk")
+            locked_relics.add("SymbioticVirus")
+            locked_relics.add("Metronome")
+            
+        if not self.is_epoch_discovered("DEFECT6_EPOCH"):
+            locked_relics.add("GoldPlatedCables")
+            locked_relics.add("EmotionChip")
+            locked_relics.add("PowerCell")
+
+        return locked_relics
--- src/run_manager.py
+++ src/run_manager.py
@@ -1,7 +1,28 @@
+from collections import defaultdict
+from typing import List, Dict
+from rng import Rng
+
+from relics import RelicsManager
+
+
+def group_relics_by_rarity_and_shuffle(relics: List[str], rng: Rng) -> Dict:
+    relics_by_rarity = defaultdict(list)
+    for relic in relics:
+        relics_by_rarity[relic.rarity].append(relic)
+    for rarity in relics_by_rarity.keys():
+        rng.up_front.unstable_shuffle(relics_by_rarity[rarity])
+    return relics_by_rarity
+
 class RunManager:
     @staticmethod
     def populate_relics(run_state):
-        pass
+        locked_relics = run_state.progress.get_locked_relics()
+        shared_relics = RelicsManager.get_shared_relics() - locked_relics
+        character_relics = RelicsManager.get_character_relics(run_state.character) - locked_relics
+        player_relics = shared_relics + character_relics
+
+        run_state.shared_relic_grab_bag = group_relics_by_rarity_and_shuffle(shared_relics, run_state.rng_set.up_front)        
+        run_state.player_relic_grab_bag = group_relics_by_rarity_and_shuffle(player_relics, run_state.rng_set.up_front)
 
     @staticmethod
     def generate_rooms(run_state):
