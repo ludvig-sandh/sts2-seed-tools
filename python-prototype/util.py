8a4735a55067723873f12d19120b0b55fbc2dc60
Ludvig Sandh <givdul11@hotmail.se>
2026-03-19 21:37:57 +0100

    feature: add preprocessing function to canonicalize input seeds

--- python-prototype/util.py
+++ python-prototype/util.py
@@ -13,3 +13,10 @@ def get_deterministic_hash_code(s: str) -> int:
         num2 = int32(num2 * 33 ^ ord(s[i + 1]))
 
     return int32(num + int32(num2 * 1566083941))
+
+def preprocess_seed(seed: str) -> str:
+    seed = seed.upper()
+    seed = seed.replace('O', '0')
+    seed = seed.replace('I', '1')
+    seed = seed.strip()
+    return seed
